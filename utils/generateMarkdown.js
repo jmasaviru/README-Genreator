404b3f451a3de396679bccdb153a834427b7ac8f
James Masaviru <jmasaviru@gmail.com>
2020-11-27 02:45:57 -0500

    Updating Markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,39 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Licence Badge
+  ![License badge](https://img.shields.io/badge/license-${data.license}-green)
+  
+  ## Table of Contents
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [Licence](#licence)
+  4. [Tests](#testing)
+  5. [Contribution](#contribution)
+  6. [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Licence
+  ${data.license}
+  *  ![License badge](https://img.shields.io/badge/license-${data.license}-green)
+ 
+  ## Tests
+  ${data.testing}
+
+  ## Contribution
+  ${data.contribution}
+  
+  ## Questions
+  * If you have questions, email me at: ${data.email},
+  * GitHub Profile link: https://github.com/${data.gitName}
 `;
 }
 
